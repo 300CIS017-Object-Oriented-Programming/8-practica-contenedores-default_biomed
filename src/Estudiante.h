530540fcd704f5b6387a15ac61c225bc6aea5cab
Camilo <camilosalazar@javerianacali.edu.co>
2025-03-26 10:46:09 -0500

    definicion de clase Estudiante

--- src/Estudiante.h
+++ src/Estudiante.h
@@ -4,10 +4,29 @@
 
 #ifndef ESTUDIANTE_H
 #define ESTUDIANTE_H
+#include <vector>
 
+#include "Tarea.h"
 
 
 class Estudiante {
+private:
+    std::vector<Tarea> tareas;
+    std::string nombre;
+    int codigoID;
+    int semestre;
+    int edad;
+    std::string carrera;
+public:
+    Estudiante();
+    ~Estudiante();
+    void mostrarInfoEstudiante();
+    int getCodigo();
+    void RegistrarTarea();
+    void EliminarTarea();
+    void ConteoTarea();
+    void MostrarTareas();
+    int getTiempoTareas();
 
 };
 
